8eea3001030c6ecbe7b9f98b88861b475e6618da
lukasz-kukulka <qqla83@gmail.com>
2021-09-17 15:14:49 +0100

    After Time

---
+++ tests.cpp
@@ -0,0 +1,18 @@
+#include "catch_amalgamated.hpp"
+
+#include <algorithm>
+#include <vector>
+
+// SCENARIO("Check sum") {
+//     GIVEN("function") {
+//         Calculator calc;
+
+//         WHEN("4") {
+//             auto result = calc.aaa(4);
+
+//             THEN("check single value in vector") {
+//                 REQUIRE(result == 4);
+//             }
+//         }
+//     }
+// }
