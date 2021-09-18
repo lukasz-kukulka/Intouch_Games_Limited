d4d571e7ccc0cd1de9d991d85abd61bedc2a6690
lukasz-kukulka <qqla83@gmail.com>
2021-09-18 23:23:26 +0100

    Tests without delimiters

--- tests.cpp
+++ tests.cpp
@@ -4,16 +4,80 @@
 #include <algorithm>
 #include <vector>
 
-SCENARIO("Check Add function" "[Sum][Add]") {
-    GIVEN("function") {
+SCENARIO("Check Add function" "[Sum][Add][WithoutDelimiter]") {
+    GIVEN("Give class calc") {
         Calculator calc;
 
-        WHEN("Give data without delimeter") {
+        WHEN("Call function Add without delimeter with string = 4,4") {
             auto result = calc.Add("4,4");
         
             THEN("Add numbers") {
                 REQUIRE(result == 8);
             }
         }
+
+        WHEN("Call function Add without delimeter with string = 22,15,45") {
+            auto result = calc.Add("22,15,45");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 82);
+            }
+        }
+
+        WHEN("Call function Add without delimeter with string = 12\n12,12") {
+            auto result = calc.Add("12\n12,12");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 36);
+            }
+        }
+
+        WHEN("Call function Add without delimeter with string = 10000,22") {
+            auto result = calc.Add("10000,22");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 22);
+            }
+        }
+        
+    }
+}
+
+SCENARIO("Check Add function" "[Sum][Add][SingleDelimiter]") {
+    GIVEN("Give class calc") {
+        Calculator calc;
+
+        WHEN("Call function Add with single delimeter with string = 4,4") {
+            auto result = calc.Add("4,4");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 8);
+            }
+        }
+
+        WHEN("Call function Add with single delimeter with string = ") {
+            auto result = calc.Add("22,15,45");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 82);
+            }
+        }
+
+        WHEN("Call function Add with single delimeter with string = ") {
+            auto result = calc.Add("12\n12,12");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 36);
+            }
+        }
+
+        WHEN("Call function Add with single delimeter with string = ") {
+            auto result = calc.Add("10000,22");
+        
+            THEN("Add numbers") {
+                REQUIRE(result == 22);
+            }
+        }
+        
     }
 }
